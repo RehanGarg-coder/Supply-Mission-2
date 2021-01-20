885cfdd40a4a56d00f33f0595af25c77af443edc
RehanGarg-coder <74549659+RehanGarg-coder@users.noreply.github.com>
2021-01-20 20:42:56 +0530

    Add files via upload

---
+++ SupplyMission-master/sketch.js
@@ -0,0 +1,78 @@
+var helicopterIMG, helicopterSprite, packageSprite,packageIMG;
+var packageBody,ground;
+
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Body = Matter.Body;
+
+function preload()
+{
+	helicopterIMG=loadImage("helicopter.png")
+	packageIMG=loadImage("package.png")
+}
+
+function setup() {
+	createCanvas(800, 700);
+	rectMode(CENTER);
+	
+
+	packageSprite=createSprite(width/2, 80, 10,10);
+	packageSprite.addImage(packageIMG)
+	packageSprite.scale=0.2
+
+	helicopterSprite=createSprite(width/2, 200, 10,10);
+	helicopterSprite.addImage(helicopterIMG)
+	helicopterSprite.scale=0.6
+
+	groundSprite=createSprite(width/2, height-35, width,10);
+	groundSprite.shapeColor=color("white")
+
+	redBlock1Sprite=createSprite(400,665,200,15);
+	redBlock1Sprite.shapeColor=color("red");
+
+	redBlock2Sprite=createSprite(300,615,15,110);
+	redBlock2Sprite.shapeColor=color("red");
+
+	redBlock3Sprite=createSprite(500,615,15,110);
+	redBlock3Sprite.shapeColor=color("red");
+
+
+	engine = Engine.create();
+	world = engine.world;
+
+	packageBody = Bodies.circle(width/2 , 200 , 5 , {restitution:0.5, isStatic:true});
+	World.add(world, packageBody);
+	
+
+	//Create a Ground
+	ground = Bodies.rectangle(width/2, 650, width, 10 , {isStatic:true} );
+	World.add(world, ground);
+	 
+	redBlock1 = Bodies.rectangle(400, 650, 200, 15 , {isStatic:true} );
+	World.add(world, redBlock1);
+
+	Engine.run(engine);
+  
+}
+
+
+function draw() {
+  rectMode(CENTER);
+  background(0);
+  packageSprite.x= packageBody.position.x 
+  packageSprite.y= packageBody.position.y 
+  drawSprites();
+ 
+}
+
+function keyPressed() {
+ if (keyCode === DOWN_ARROW) {
+    // Look at the hints in the document and understand how to make the package body fall only on press of the Down arrow key.
+     Matter.Body.setStatic(packageBody,false);
+    
+  }
+}
+
+
+
